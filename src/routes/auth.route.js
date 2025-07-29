ca5bc006ab8ca3b7e5d59f5de134841aadc31830
suman kar <enter>
2025-07-30 01:26:42 +0530

    update api

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,40 +1,11 @@
 const express = require('express');
-const userModel = require('../models/user.model');
-const jwt = require('jsonwebtoken');
+const { registerController, loginController } = require('../controllers/auth.controller');
 const router = express.Router()
 
 // register 
-router.post("/register",async(req,res)=>{
-    const {username,password} = req.body
 
-    const existingUser = await userModel.findOne({
-        username
-    })
+router.post("/register",registerController)
 
-    if(existingUser){
-        return res.status(409).json({
-            message: "User already exists"
-        })
-    }
-
-    const user = await userModel.create({
-        username,
-        password
-    })
-
-    const token = jwt.sign({
-        id:user._id,
-
-    },process.env.JWT_SECRET)
-
-    res.cookie("token",token)
-
-
-    res.status(201).json({
-        message:"User Created Successfully",
-        user
-    })
-})
-
+router.post("/login",loginController)
 
 module.exports = router
