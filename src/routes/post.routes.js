3b21242e6a2e6b711cc1914b369526d3c1ca5fe6
suman kar <enter>
2025-07-31 20:42:47 +0530

    update

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,6 +1,12 @@
 const express= require('express');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
+const multer = require('multer');
+
+
+const upload = multer({
+    storage:multer.memoryStorage()
+})
 
 
 
